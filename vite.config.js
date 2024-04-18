a4ee765031ea62dfcde4329606f97ded0d409000
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-04-18 22:40:37 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ htmlFiles.forEach((file) => {
   input[file.replace('.html', '')] = resolve(__dirname, 'src', file);
 });
 
-export default defineConfig({
+/* export default defineConfig({
   base: '/AgriVisionTech',
   root: 'src',
   publicDir: '../public',
@@ -20,6 +20,21 @@ export default defineConfig({
     rollupOptions: {
       input,
     },
+    outDir: '../dist/',
+    emptyOutDir: true,
+  },
+}); */
+
+export default defineConfig({
+  base: '/AgriVisionTech',
+  root: 'src',
+  publicDir: '../public',
+  plugins: [
+    handlebars({ partialDirectory: resolve(__dirname, 'src/templates') }),
+  ],
+  build: {
+    minify: false,
+    cssMinify: false,
     outDir: '../dist/',
     emptyOutDir: true,
   },
